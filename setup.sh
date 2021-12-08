bc6d143bbbea0e1eb9c7fd8b40f9bc1fa20dbca6
Myles-Mattlock <65444145+Myles-Mattlock@users.noreply.github.com>
2021-12-08 15:43:42 +0000

    Fixed some bugs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Change Debian to SID Branch
-#cp /etc/apt/sources.list /etc/apt/sources.list.bak
-#cp sources.list /etc/apt/sources.list 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp sources.list /etc/apt/sources.list 
 
 # Update packages list
 sudo apt update
