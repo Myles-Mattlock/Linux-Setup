9a96e7c273cb5d96f4a26a40ef4c71c3211c6d74
Myles-Mattlock <65444145+Myles-Mattlock@users.noreply.github.com>
2021-12-08 14:16:08 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Change Debian to SID Branch
+#cp /etc/apt/sources.list /etc/apt/sources.list.bak
+#cp sources.list /etc/apt/sources.list 
+
+# Update packages list
+sudo apt update
+
+# Add base packages
+sudo apt install unzip picom neofetch git htop pulseeffects pavucontrol nemo papirus-icon-theme fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Make Theme folders
+mkdir -p ~/.themes ~/.fonts
+
+# Fira Code Nerd Font variant needed
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d ~/.fonts   
+fc-cache -vf
+
+#Ms-fonts
+sudo apt install ttf-mscorefonts-installer
+
+# Layan Cursors
+cd "$HOME/build"
+git clone https://github.com/vinceliuice/Layan-cursors
+cd Layan-cursors
+sudo ./install.sh
